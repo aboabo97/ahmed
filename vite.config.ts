27070481ace3db1bad1d624ff101107606024964
fatimaalikrm <202108358@stu.uob.edu.bh>
2025-04-26 19:18:39 +0300

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  root: resolve(__dirname),
+  publicDir: resolve(__dirname, 'public'),
+  build: {
+    outDir: resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        events: resolve(__dirname, 'public/events.html'),
+        createEvent: resolve(__dirname, 'public/create-event.html'),
+        eventDetails: resolve(__dirname, 'public/event-details.html'),
+      },
+    },
+  },
+});
